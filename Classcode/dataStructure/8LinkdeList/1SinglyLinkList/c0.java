5d95b3d54cf3f99ad080be8deabc65e335dc3782
Amar_raj <amarthonge6767@gmail.com>
2023-12-13 00:44:02 +0530

    rem

---
+++ Classcode/dataStructure/8LinkdeList/1SinglyLinkList/c0.java
@@ -0,0 +1,35 @@
+
+
+class Node{
+
+	int data;
+	Node next = null;
+
+	Node(int data){
+	
+		this.data = data;
+	}
+
+}
+
+class LinkedList{
+
+	Node head = null;
+	void addFirst(int data){
+	
+		Node newNode = new Node(10);
+		head = newNode;
+	}
+
+
+}
+
+
+class client{
+
+	public static void main(String[] boss){
+	
+
+	}
+
+}
